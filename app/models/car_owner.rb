eabba620aca3adf2c605c2c35a8d2f7f1e8faf06
badogan <basri.dogan.71@gmail.com>
2019-11-25 12:14:35 +0000

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,8 +2,34 @@ class CarOwner
 
   attr_reader :name
 
-  def initialize(name)
+  @@all = []
+  
+  def initialize(name:)
     @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars_belong_to
+    #Get a list of all the cars that a specific owner has
+    Car.all.select{|car|car.owner == self}
+  end
+
+  def mechanics_visited
+    #Get a list of all the mechanics that a specific owner goes to
+    Car.all.select{|car|car.owner == self}.map{|car|car.mechanic}
+  end
+
+  def self.average_cars_owned
+    #Get the average amount of cars owned for all owners
+    Car.all.count.to_f/CarOwner.all.count.to_f
   end
 
 
