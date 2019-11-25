09c130fe47d703ffef2637d1aa913645d03339b1
Sara Fotros <sarafotros@users.noreply.github.com>
2019-11-25 16:33:29 +0000

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,47 @@
 class CarOwner
 
-  attr_reader :name
+  attr_reader :name 
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+   
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select{|car| car.car_owner == self}
+  end
+
+  def mechanics
+    cars.map{|car| car.mechanic}
+  end
+
+  def self.average_cars
+    Car.all.length.to_f / self.all.length
   end
 
 
+  
+
+
+ 
+
+
+
+  
 end
+
+
+
+#  def get_mechanic(car)
+#   Mechanic.new(car:car, car_owner:self)
+#  end
+
+ # def buy_car(make:, model:, classification:, mechanic:)
+  #   new_car = Car.new(make:make, model:model, classification:classification, 
+  # end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,21 @@
 require_relative '../config/environment.rb'
 
+
 def reload
   load 'config/environment.rb'
 end
 
+
+owner1 = CarOwner.new("Jack")
+owner2 = CarOwner.new("Sara")
+
+mec1 = Mechanic.new( "Supercars",  "Ian")
+mec2 = Mechanic.new("Alle", "Vintage")
+mec3 = Mechanic.new("Amir", "Vintage")
+
+car1 = Car.new("Ford", "Fiesta", "Vintage", owner1, mec2)
+car2 = Car.new("Ferrari", "F40", "Vintage", owner2, mec3)
+car3 = Car.new("Porshce", "911", "Supercars", owner1, mec1)
+
 binding.pry
+0
